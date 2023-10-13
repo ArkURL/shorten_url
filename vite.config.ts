3d866ba0196e7cc523ce6bcfacae5d9181aa0be3
ui_none <1035096867@qq.com>
2023-10-13 17:36:39 +0800

    feat: 引入element的plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,35 @@
-import { defineConfig } from 'vite'
-import {resolve} from 'path'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import AutoImport from "unplugin-auto-import/vite"
+import Components from "unplugin-vue-components/vite"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import { resolve } from "path"
+import vue from "@vitejs/plugin-vue"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src")
-    }
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://cleanuri.com",
-        changeOrigin: true,
-        rewrite(path) {
-            return path
+    plugins: [
+        vue(),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": resolve(__dirname, "src"),
         },
-      }
-    }
-  }
+    },
+    server: {
+        proxy: {
+            "/api": {
+                target: "https://cleanuri.com",
+                changeOrigin: true,
+                rewrite(path) {
+                    return path
+                },
+            },
+        },
+    },
 })
