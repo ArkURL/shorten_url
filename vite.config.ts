b8786ed17d20b404a77a83a6c809710d90035bfc
ui_none <1035096867@qq.com>
2023-10-14 00:34:09 +0800

    chore: 添加element-icon的vite设置

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,48 @@
 import { defineConfig } from "vite"
 import AutoImport from "unplugin-auto-import/vite"
+import Icons from "unplugin-icons/vite"
+import IconsResolver from "unplugin-icons/resolver"
 import Components from "unplugin-vue-components/vite"
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 import { resolve } from "path"
 import vue from "@vitejs/plugin-vue"
+
+const pathSrc = resolve(__dirname, "src")
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
         AutoImport({
-            resolvers: [ElementPlusResolver()],
+            resolvers: [
+                ElementPlusResolver(), // Auto import icon components
+                // 自动导入图标组件
+                IconsResolver({
+                    prefix: "Icon",
+                }),
+            ],
+            dts: "auto-imports.d.ts",
         }),
         Components({
-            resolvers: [ElementPlusResolver()],
+            resolvers: [
+                // Auto register icon components
+                // 自动注册图标组件
+                IconsResolver({
+                    enabledCollections: ["ep"],
+                }),
+                // Auto register Element Plus components
+                // 自动导入 Element Plus 组件
+                ElementPlusResolver(),
+            ],
+            dts: "components.d.ts",
+        }),
+        Icons({
+            autoInstall: true,
         }),
     ],
     resolve: {
         alias: {
-            "@": resolve(__dirname, "src"),
+            "@": pathSrc,
         },
     },
     server: {
