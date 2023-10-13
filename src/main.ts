78565df13ad45b733c04b1436014b8f1d51e5758
ui_none <1035096867@qq.com>
2023-10-13 17:36:03 +0800

    refactor: 将app提取出来，从而添加模态库plugin

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue"
+import { LoadingPlugin } from "vue-loading-overlay"
+import "vue-loading-overlay/dist/css/index.css"
+import "./style.css"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(LoadingPlugin)
+app.mount("#app")
