819dc44be2fba0181f25ccdf348f0acb2e71aae9
ui_none <1035096867@qq.com>
2023-10-23 14:27:52 +0800

    fix: 添加favicon

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module "vue" {
     export interface GlobalComponents {
         ElAlert: (typeof import("element-plus/es"))["ElAlert"]
-        ElButton: (typeof import("element-plus/es"))["ElButton"]
         ElIcon: (typeof import("element-plus/es"))["ElIcon"]
         ElPopover: (typeof import("element-plus/es"))["ElPopover"]
         VerticalTextBoard: (typeof import("./src/components/VerticalTextBoard.vue"))["default"]
